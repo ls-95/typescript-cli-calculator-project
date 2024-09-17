ad05c414a6a655b430eaf7353f91a1642df2b4eb
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-09-17 10:52:05 +0200

    Added function to convert string to numbers and check if a number

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ function main() {
     var firstStr = (0, readline_sync_1.question)("Enter first number:\n ");
     var operator = (0, readline_sync_1.question)("Enter operator:\n");
     var secondStr = (0, readline_sync_1.question)("Enter second number:\n");
-    console.log(firstStr, operator, secondStr);
+    //Need to convert the strings into numbers for JavaScript, as otherwise a string '22' + '11' will be 2211. isNumber function created.
+    var firstNum = isNumber(firstStr);
+    console.log(firstNum);
+}
+function isNumber(str) {
+    //Returns a boolean as we want to know if the string is a number, true or false.
+    var maybeNum = parseInt(str); //Parses a string argument and returns an integer(whole number).
+    var isNum = !isNaN(maybeNum);
+    return isNum;
+    //maybeNum = 456 (because "456" can be turned into the number 456)
+    //isNaN(456) is false (because it's a valid number), so !false becomes true.
+    //The code will return true.
 }
 main();
--- index.ts
+++ index.ts
@@ -4,8 +4,21 @@ function main(): void {
   const firstStr: string = question("Enter first number:\n ");
   const operator: string = question("Enter operator:\n");
   const secondStr: string = question("Enter second number:\n");
+  //Need to convert the strings into numbers for JavaScript, as otherwise a string '22' + '11' will be 2211. isNumber function created.
 
-  console.log(firstStr, operator, secondStr);
+  const firstNum = isNumber(firstStr);
+  console.log(firstNum);
+}
+
+function isNumber(str: string): boolean {
+  //Returns a boolean as we want to know if the string is a number, true or false.
+  const maybeNum = parseInt(str); //Parses a string argument and returns an integer(whole number).
+  const isNum: boolean = !isNaN(maybeNum);
+  return isNum;
+
+  //maybeNum = 456 (because "456" can be turned into the number 456)
+  //isNaN(456) is false (because it's a valid number), so !false becomes true.
+  //The code will return true.
 }
 
 main();
