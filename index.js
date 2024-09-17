133406fb4bf6f88ab404453fe03532cdd9217b68
ls-95 <143622355+ls-95@users.noreply.github.com>
2024-09-17 10:12:29 +0200

    Created firstStr, operator and secondStr

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var readline_sync_1 = require("readline-sync");
 function main() {
     var firstStr = (0, readline_sync_1.question)("Enter first number:\n ");
-    console.log(firstStr);
+    var operator = (0, readline_sync_1.question)("Enter operator:\n");
+    var secondStr = (0, readline_sync_1.question)("Enter second number:\n");
+    console.log(firstStr, operator, secondStr);
 }
 main();
--- index.ts
+++ index.ts
@@ -2,7 +2,10 @@ import { question } from "readline-sync";
 
 function main(): void {
   const firstStr: string = question("Enter first number:\n ");
-  console.log(firstStr);
+  const operator: string = question("Enter operator:\n");
+  const secondStr: string = question("Enter second number:\n");
+
+  console.log(firstStr, operator, secondStr);
 }
 
 main();
